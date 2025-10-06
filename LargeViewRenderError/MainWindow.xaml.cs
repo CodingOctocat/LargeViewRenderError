6b6ebbfb83b68e423befb01335c2c4da4a61ad7c
CodingNinja <CodingNinja@outlook.com>
2025-10-06 10:04:23 +0800

    添加项目文件。

---
+++ LargeViewRenderError/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace LargeViewRenderError;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private AdornerLayer _adornerLayer = null!;
+
+    private bool _isSelecting;
+
+    private SelectionBoxAdorner? _selectionBoxAdorner;
+
+    private Point _startPoint;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    // 鼠标按下事件，开始选区
+    private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        // 只有左键点击时才开始
+        if (e.ButtonState == MouseButtonState.Pressed && e.ChangedButton == MouseButton.Left)
+        {
+            _startPoint = e.GetPosition(canvas);
+            _isSelecting = true;
+
+            // 创建 SelectionBoxAdorner 并添加到 AdornerLayer
+            _selectionBoxAdorner = new SelectionBoxAdorner(canvas, _startPoint);
+            _adornerLayer.Add(_selectionBoxAdorner);
+        }
+    }
+
+    // 鼠标移动事件，更新选区大小
+    private void Canvas_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (_isSelecting && _selectionBoxAdorner != null)
+        {
+            var currentPoint = e.GetPosition(canvas);
+            _selectionBoxAdorner.UpdateEndPoint(currentPoint);
+            txtX.Text = $"X: {currentPoint.X}";
+            txtY.Text = $"Y: {currentPoint.Y}";
+            Debug.WriteLine(">>> " + currentPoint);
+        }
+    }
+
+    // 鼠标松开事件，结束选区
+    private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        if (_isSelecting)
+        {
+            _isSelecting = false;
+
+            if (_selectionBoxAdorner != null)
+            {
+                _adornerLayer.Remove(_selectionBoxAdorner);
+            }
+        }
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        _adornerLayer = AdornerLayer.GetAdornerLayer(canvas); // 获取 AdornerLayer
+    }
+}
---
+++ LargeViewRenderError/SelectionBoxAdorner.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace LargeViewRenderError;
+
+public class SelectionBoxAdorner : Adorner
+{
+    private readonly Pen _border;
+
+    private Point _end;
+
+    private Point _start;
+
+    public Brush BorderBrush { get; set; } = Brushes.DodgerBlue;
+
+    public Brush Fill { get; set; }
+
+    public SelectionBoxAdorner(UIElement adornedElement, Point start) : base(adornedElement)
+    {
+        BorderBrush = Brushes.DodgerBlue;
+        BorderBrush.Freeze();
+        Fill = new SolidColorBrush(Color.FromArgb(50, 0, 120, 215));
+        Fill.Freeze();
+        _border = new(BorderBrush, 1);
+
+        _start = start;
+        _end = start;
+        IsHitTestVisible = false;
+
+        // 测试
+        UseLayoutRounding = true;
+        SnapsToDevicePixels = true;
+    }
+
+    public Rect GetSelectionRect()
+    {
+        return new Rect(_start, _end);
+    }
+
+    public void UpdateEndPoint(Point end)
+    {
+        _end = end;
+        _end.X = Math.Max(_end.X, 0);
+        _end.Y = Math.Max(_end.Y, 0);
+        InvalidateVisual();
+    }
+
+    protected override void OnRender(DrawingContext dc)
+    {
+        var rect = GetSelectionRect();
+        dc.DrawRectangle(Fill, _border, rect);
+    }
+}
